e043cdd47def7638d45b1a5395b148f4a5133e27
charlesfeatherstone <charlesfstone11@gmail.com>
2024-11-20 13:23:16 -0500

    NavBar Edit, and CSS Edit

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,14 +1,28 @@
 import { Link } from "react-router-dom"
 import { useState } from "react"
 
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+fetch(imgUrl)
+    .then(function(response)
+    {
+    return response.json
+    })
+    .then(function(data){
+        const array=data.message
+        console.log(array)
+    })
+
 
 function Navbar(){
     return(
-        <div>
-            <div className="nav_menu">
-                <Link to="/message-list">About Me</Link>
+        <div className="nav_menu">
+            <div className="name_content">
+                <div>Charles Featherstone</div>
+                </div>
+            <div className="nav_menu_content">
+                <Link to="/message-list">About</Link>
                 <Link to="/search">Projects</Link>
-                <Link to="/my-profile">FAQ</Link>
+                <Link to="/my-profile">Contact</Link>
                 <a href="/">Home</a>
             </div>
         </div>
